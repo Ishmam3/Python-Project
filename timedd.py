599f9598971b7d39f7356d9312a7d97f43524d04
Ishmam3 <ishmam8910@gmail.com>
2024-09-20 00:49:28 +0600

    Update timedd.py

--- timedd.py
+++ timedd.py
@@ -5,6 +5,11 @@ def update():
     time_str = strftime("%I:%M:%S")
     time_label.config(text=time_str)
     
+    date_str = strftime("%A")
+    day_label.config(text=date_str)
+    
+    date_str = strftime("%B %d, %Y")
+    date_label.config(text=date_str)
     window.after(1000, update)
 
 
@@ -12,6 +17,12 @@ window = Tk()
 
 time_label = Label(window,font=("Arial",50), fg="#00ff00", bg="#111111")
 time_label.pack()
+
+day_label = Label(window, font=("Ink Free", 25))
+day_label.pack()
+
+date_label = Label(window, font=("Ink Free", 25))
+date_label.pack()
 
 update()
 
